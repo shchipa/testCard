c6ef2b669f3f5b7b74ab84e7f8c6e5b8c9a11f47
shchipa <shchipaa@mail.ru>
2017-08-10 13:40:58 +0300

    Add files via upload

--- back.php
+++ back.php
@@ -1,33 +1,21 @@
 <?php
 
 include 'config.php';
+include 'classes/Data.php';
 include 'classes/Database.php';
 
-
-class Back {
-
-    public function get_data_contract_db($id_contract){
-        $db = new Database(HOST, USER, PASS, DB);
-        $result = $db->get_data_contract($id_contract);
-
-        return $result;
-    }
-
-//    public function get_view($data){
-//    }
-}
 
 //if(isset($_POST['id_contract'])) {
     $id_contract = $_POST['id_contract'];
 
 
-    $back = new Back();
-    $data = $back->get_data_contract_db($id_contract);
+    $data = new Data();
+    $data_contr = $data->get_data_contract_db($id_contract);
 //}
 
 //$view = $back->get_view($data);
 
 //вернуть $data через json
-echo json_encode($data);
+echo json_encode($data_contr);
 
 ?>
